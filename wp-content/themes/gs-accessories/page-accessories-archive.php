1eff54d4b9070e97d178927edfc53f8b00dd6aff
Leon Magee <leonmagee33@gmail.com>
2018-01-10 09:02:03 -0800

    update

--- wp-content/themes/gs-accessories/page-accessories-archive.php
+++ wp-content/themes/gs-accessories/page-accessories-archive.php
@@ -9,52 +9,45 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<div class="max-width-wrap accessories-archive">
-			<main id="main" class="site-main">
-
-                <?php
-
-                $args = array('post_type' => 'accessories');
-                $accessories_query = new WP_Query($args);    
-                while ( $accessories_query->have_posts() ) {
-                    $accessories_query->the_post(); ?>
+<div id="primary" class="content-area">
+  <div class="max-width-wrap accessories-archive">
+   <main id="main" class="site-main">
+    <div class="grid-x">
+        <?php
+        $args = array('post_type' => 'accessories');
+        $accessories_query = new WP_Query($args);    
+        while ( $accessories_query->have_posts() ) {
+            $accessories_query->the_post(); ?>
+            <div class="small-6 medium-4 cell">
+                <a href="<?php the_permalink(); ?>">
                     <div class="accessorie-archive-item">
-                        <a href="<?php the_permalink(); ?>">
-                            <div class="grid-x">
-                                <div class="cell medium-3 large-2 image-wrap">
-                                    <?php 
+                        <div class="archive-item-img-bg">
 
-                                    $image_gallery = get_field('image_gallery');
-                                    if ( $image_gallery && $img_featured = array_shift($image_gallery)) { ?>
-                                        
-                                        <img src="<?php echo $img_featured['sizes']['medium']; ?>" />
-                                    <?php
+                            <?php 
 
-                                    } else { ?>
-                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/placeholder-image-small.jpg" />
-                                    <?php } ?>
-                                </div>
-                                <div class="cell medium-9 large-10 content-wrap">
-                                    <h2><?php the_title(); ?></h2>
-                                    <p><?php echo content_excerpt(get_field('accessory_text'),500); ?></p>
-                                </div>
-                            </div>
-                        </a>
+                            $image_gallery = get_field('image_gallery');
+                            if ( $image_gallery && $img_featured = array_shift($image_gallery)) { ?>
+
+                            <img src="<?php echo $img_featured['sizes']['medium']; ?>" />
+
+                            <?php
+
+                        } else { ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/placeholder-image-small.jpg" />
+                        <?php } ?>
                     </div>
-                <?php }            
 
-
-				// while ( have_posts() ) : the_post();
-
-				// 	get_template_part( 'template-parts/content', 'page' );
-
-				// endwhile; // End of the loop.
-				?>
-
-			</main><!-- #main -->
-		</div>
-	</div><!-- #primary -->
+                    <div class="archive-item-title">
+                        <h2><?php the_title(); ?></h2>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <?php } ?>
+    </div>
+</main><!-- #main -->
+</div>
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
