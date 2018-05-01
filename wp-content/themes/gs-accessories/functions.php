37eb02f5e462bc8b3d063914787c20400c351674
Leon Magee <leonmagee33@gmail.com>
2018-05-01 09:25:10 -0700

    update

--- wp-content/themes/gs-accessories/functions.php
+++ wp-content/themes/gs-accessories/functions.php
@@ -261,7 +261,7 @@ function gs_accessories_custom_scripts() {
 	wp_enqueue_script( 'foundation-init-js' );
 
 	
-	wp_register_style( 'gs-accessories-styles', get_template_directory_uri() . '/assets/css/main.min.css', '', '1.0.13' );
+	wp_register_style( 'gs-accessories-styles', get_template_directory_uri() . '/assets/css/main.min.css', '', '1.0.14' );
 	
 	wp_enqueue_style( 'gs-accessories-styles' );
 
