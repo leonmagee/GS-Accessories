827846dbbf7d95cea675a78fbae29430be9f9d39
Leon Magee <leonmagee33@gmail.com>
2018-05-13 21:52:50 -0700

    update

--- wp-content/themes/gs-accessories/js/custom-admin.js
+++ wp-content/themes/gs-accessories/js/custom-admin.js
@@ -16,7 +16,7 @@ jQuery(function ($) {
         success.hide();
         alert.hide();
 
-        var rest_url = 'https://mygsaccessories.com/wp-admin/edit.php?post_type=orders/wp-json/process_emails/admin/' + post_id + '/' + email_address;
+        var rest_url = 'https://mygsaccessories.com/wp-json/process_emails/admin/' + post_id + '/' + email_address;
 
         $.ajax({
             type: 'GET',
@@ -61,7 +61,7 @@ jQuery(function ($) {
         success.hide();
         alert.hide();
 
-        var rest_url = 'https://mygsaccessories.com/wp-admin/edit.php?post_type=orders/wp-json/process_emails/user/' + post_id + '/' + email_address + '/' + tracking_number;
+        var rest_url = 'https://mygsaccessories.com/wp-json/process_emails/user/' + post_id + '/' + email_address + '/' + tracking_number;
 
         $.ajax({
             type: 'GET',
