5165037e20c9a355071c86b9a8a005f1abd8e008
Leon Magee <leonmagee33@gmail.com>
2018-06-15 16:18:43 -0700

    update

--- wp-content/themes/gs-accessories/page-inventory-report-template.php
+++ wp-content/themes/gs-accessories/page-inventory-report-template.php
@@ -7,54 +7,58 @@ restricted_page();
 get_header(); 
 ?>
 
-	<div id="primary" class="content-area">
-		<div class="max-width-wrap">
-			<main id="main" class="site-main">
+<div id="primary" class="content-area">
+	<div class="max-width-wrap">
+		<main id="main" class="site-main">
 
-				<h1 class="entry-title">Inventory Report</h1>
+			<h1 class="entry-title">Inventory Report</h1>
+
+			<div class="accessory-inventory-wrap">
 
 				<?php
 
-					$args = array('post_type' => 'accessories');
-					$custom_query = new WP_Query($args);
-					while( $custom_query->have_posts() ) {
-						$custom_query->the_post();
+				$args = array('post_type' => 'accessories');
+				$custom_query = new WP_Query($args);
+				while( $custom_query->have_posts() ) {
+					$custom_query->the_post();
 
-						?>
-						
-						    <?php $color_quantity = get_accessory_colors();
-                            if ( $color_quantity ) { ?>
-                            	<div class="accessory-inventory-item">
-								<h4><?php the_title(); ?></h4>
-                                <table class="single-accessory-table">
-                                    <thead>
-                                        <tr>
-                                            <th>Color</th>
-                                            <th>In Stock</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+					?>
+					
+					<?php $color_quantity = get_accessory_colors();
+					if ( $color_quantity ) { ?>
+						<div class="accessory-inventory-item">
+							<h4><?php the_title(); ?></h4>
+							<table class="single-accessory-table">
+								<thead>
+									<tr>
+										<th>Color</th>
+										<th>In Stock</th>
+									</tr>
+								</thead>
+								<tbody>
 
-                                <?php foreach( $color_quantity as $item => $quantity ) { ?>
-                                    <tr>
-                                        <td><?php echo $item; ?></td>
-                                        <td><?php echo $quantity; ?> Available</td>
-                                    </tr>
-                                <?php } ?>
+									<?php foreach( $color_quantity as $item => $quantity ) { ?>
+										<tr>
+											<td><?php echo $item; ?></td>
+											<td><?php echo $quantity; ?> Available</td>
+										</tr>
+									<?php } ?>
 
-                                    </tbody>
-                                </table>
-                                						</div>
+								</tbody>
+							</table>
+						</div>
 
-                            <?php } 
-					 }
-					wp_reset_postdata();
+					<?php } 
+				}
+				wp_reset_postdata();
 
 				?>
 
-			</main><!-- #main -->
-		</div>
-	</div><!-- #primary -->
+			</div>
+
+		</main><!-- #main -->
+	</div>
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
