8e59a7c82d6da45248b57f2fca9be1ef0ace06f4
Leon Magee <leonmagee33@gmail.com>
2018-06-21 13:50:09 -0700

    update

--- wp-content/themes/gs-accessories/header.php
+++ wp-content/themes/gs-accessories/header.php
@@ -101,7 +101,6 @@
 								<?php } else { ?>
 
 								<li><a href="/place-your-order">Add to Order</a></li>
-								<li><a href="/rma">Submit RMA</a></li>
 								<li><a href="/cart"><i class="fa fa-shopping-cart"></i></a></li>
 
 								<?php } ?>
