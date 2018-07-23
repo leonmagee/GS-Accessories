d04cb1dea5a67beed4fb87b2c6f86203223b3f28
Leon Magee <leonmagee33@gmail.com>
2018-07-23 15:06:28 -0700

    update

--- wp-content/themes/gs-accessories/page-homepage.php
+++ wp-content/themes/gs-accessories/page-homepage.php
@@ -10,61 +10,85 @@ get_header(); ?>
 
 <div class="homepage-outer-outer-wrap">
 
-<div class="homepage-outer-wrap">
+  <div class="homepage-outer-wrap">
 
-  <div class="homepage-slider">
+    <div class="homepage-slider">
 
-    <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+      <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
 
-      <div class="orbit-wrapper">
+        <div class="orbit-wrapper">
 
-        <ul class="orbit-container">
+          <ul class="orbit-container">
 
-          <?php $slides = get_field('homepage_slides', 'option');
+            <?php $slides = get_field('homepage_slides', 'option');
 
-          foreach( $slides as $slide ) { ?>
+            foreach( $slides as $slide ) { ?>
 
-          <li class="orbit-slide">
-            <figure class="orbit-figure">
-              <div class="accessorie-slide">
+              <li class="orbit-slide">
+                <figure class="orbit-figure">
+                  <div class="accessorie-slide">
 
-                <img src="<?php echo $slide['slide_image']['url']; ?>" />
+                    <img src="<?php echo $slide['slide_image']['url']; ?>" />
 
-              </div>
+                  </div>
 
-            </figure>
-          </li>
+                </figure>
+              </li>
 
-          <?php }?>
-        </ul>
+            <?php }?>
+          </ul>
+        </div>
       </div>
-  </div>
 
-</div>
+    </div>
 
-<div class="homepage-amazon-wrap">
-  <div class="grid-x grid-margin-x">
-    <div class="cell small-4"><a href="<?php echo site_url(); ?>/products/samsung-unlocked-gsm-cellphones"><img src="https://mygsaccessories.com/wp-content/uploads/banner-logos-samsung.png" /></a></div>
-    <div class="cell small-4"><a href="<?php echo site_url(); ?>/products/iphone-unlocked-gsm-cellphones"><img src="https://mygsaccessories.com/wp-content/uploads/banner-logos-apple.png" /></a></div>
-    <div class="cell small-4"><a href="<?php echo site_url(); ?>/products/lg-unlocked-gsm-cellphones"><img src="https://mygsaccessories.com/wp-content/uploads/banner-logos-lg.png" /></a></div>
-  </div>
+    <div class="homepage-logo-wrap">
+      <div class="grid-x grid-margin-x">
+        <div class="cell medium-4">
+          <a href="<?php echo site_url(); ?>/products/samsung-unlocked-gsm-cellphones">
+            <div class="cta-logo-item">
+              <div class="item-text">Shop For</div>
+              <img src="<?php echo site_url(); ?>/wp-content/uploads/banner-logos-samsung-grey.png" />
+              <div class="item-text">Galaxy</div>
+            </div>
+          </a>
+        </div>
+        <div class="cell medium-4">
+          <a href="<?php echo site_url(); ?>/products/iphone-unlocked-gsm-cellphones">
+            <div class="cta-logo-item">
+              <div class="item-text">Shop For</div>
+              <img src="<?php echo site_url(); ?>/wp-content/uploads/banner-logos-apple-grey.png" />
+              <div class="item-text">iPhone</div>
+            </div>
+          </a>
+        </div>
+        <div class="cell medium-4">
+          <a href="<?php echo site_url(); ?>/products/lg-unlocked-gsm-cellphones">
+            <div class="cta-logo-item">
+              <div class="item-text">Shop For</div>
+              <img src="<?php echo site_url(); ?>/wp-content/uploads/banner-logos-lg-grey.png" />
+              <div class="item-text">G Phone</div>
+            </div>
+          </a>
+        </div>
+      </div>
 <!--   <a href="https://www.amazon.com/s?marketplaceID=ATVPDKIKX0DER&me=A1AC06F3GET8FM&merchant=A1AC06F3GET8FM&redirect=true" target="_blank">
   <img src="<?php //echo site_url(); ?>/wp-content/uploads/amazon_buy_link_new.png" />
 </a>
- -->
+-->
 </div>
 
 <div class="gs-homepage-content">
 
-    <div class="max-width-wrap">
+  <div class="max-width-wrap">
 
-      <?php
-      while ( have_posts() ) : the_post();
+    <?php
+    while ( have_posts() ) : the_post();
 
-        the_content();
+      the_content();
 
-      endwhile;
-      ?>
+    endwhile;
+    ?>
 
   </div> 
 
