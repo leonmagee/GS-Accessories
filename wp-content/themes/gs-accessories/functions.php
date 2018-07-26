1565fca41c3e88ea85338d9da2b40fe0dce01597
Leon Magee <leonmagee33@gmail.com>
2018-07-26 14:35:04 -0700

    update

--- wp-content/themes/gs-accessories/functions.php
+++ wp-content/themes/gs-accessories/functions.php
@@ -363,7 +363,7 @@ function gs_accessories_scripts() {
 
 	wp_enqueue_style( 'foundation-css' );
 
-	wp_register_script( 'custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery', 'jquery-ui-core', 'jquery-ui-datepicker'), '1.2.14', true );
+	wp_register_script( 'custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery', 'jquery-ui-core', 'jquery-ui-datepicker'), '1.2.15', true );
 
 	wp_enqueue_script( 'custom-js');
 
